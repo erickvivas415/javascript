91ca4de9bb8e606ad87a092e345aba7edc57aef7
Erick Vivas <88099754+erickvivas415@users.noreply.github.com>
2022-04-12 07:58:26 -0700

    Adding Simon Game update 2

---
+++ JS1/Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,42 @@
+var buttonColours = ["red" ,"blue" ,"yellow","green"];
+
+var gamePattern = [];
+
+var userClickedPattern = [];
+
+var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', 'sounds/blue.mp3');
+
+function playAudio() { 
+    audioElement.play(); 
+}; 
+
+$(".btn").click(function(){
+    var userChosenColour = $(this).attr("id");
+    console.log(userChosenColour);
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+});
+
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random()*4);
+    console.log(randomNumber);
+
+    var randomChosenColour = buttonColours[randomNumber];
+    
+    gamePattern.push(randomChosenColour);
+
+    $("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+    
+};
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+};
+//nextSequence();
+
+
+$("body").keydown(nextSequence());
